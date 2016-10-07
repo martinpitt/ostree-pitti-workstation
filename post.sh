44883daf24125ca36aa147f96012ff902d89cb1a
Owen W. Taylor <otaylor@fishsoup.net>
2016-10-07 13:13:55 -0400

    Fix problem with systemd logs not being persisted
    During the boot with ostree, the detection of whether the logs on
    the system should be persisted can be confused. Explicitely configure
    them to be persisted.
    
    Copied from atomic-ws.

--- post.sh
+++ post.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 set -xeuo pipefail
+
+# See https://bugzilla.redhat.com/show_bug.cgi?id=1265295 ; some
+# aspects of that have been fixed, but apparently this is still
+# necessary, and generally makes things less finicky
+echo 'Storage=persistent' >> /etc/systemd/journald.conf
