1e79db4d2b4b6cd10ce497d9eae9384bffc55c68
Colin Walters <walters@verbum.org>
2017-09-13 15:56:11 -0400

    post: Re-instate systemd ProtectHome/ProtectSystem and PrivateTmp
    Following the `fedora-atomic` version:
    https://pagure.io/fedora-atomic/c/174e04cb6c268ce32135e8e70194b3ee42fb6f9d?branch=master

--- post.sh
+++ post.sh
@@ -6,6 +6,3 @@ set -xeuo pipefail
 # aspects of that have been fixed, but apparently this is still
 # necessary, and generally makes things less finicky
 echo 'Storage=persistent' >> /etc/systemd/journald.conf
-
-# Work around https://github.com/systemd/systemd/issues/4082
-find /usr/lib/systemd/system/ -type f -exec sed -i -e '/^PrivateTmp=/d' -e '/^Protect\(Home\|System\)=/d' {} \;
