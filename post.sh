c3ce622191372cd0372fec9dc3746e1c87dc8a17
Colin Walters <walters@verbum.org>
2018-01-09 16:33:30 -0500

    post.sh: Sync with fedora-atomic
     - journald.conf change tweak
     - glibc_post_upgrade removal

--- post.sh
+++ post.sh
@@ -1,8 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# This file is very similar to treecompose-post.sh
+# from fedora-atomic: https://pagure.io/fedora-atomic
+# Make changes there first where applicable.
 
 set -xeuo pipefail
 
-# See https://bugzilla.redhat.com/show_bug.cgi?id=1265295 ; some
-# aspects of that have been fixed, but apparently this is still
-# necessary, and generally makes things less finicky
-echo 'Storage=persistent' >> /etc/systemd/journald.conf
+# Work around https://bugzilla.redhat.com/show_bug.cgi?id=1265295
+# Also note the create-new-then-rename dance for rofiles-fuse compat
+if ! grep -q '^Storage=persistent' /etc/systemd/journald.conf; then
+    (cat /etc/systemd/journald.conf && echo 'Storage=persistent') > /etc/systemd.journald.conf.new
+    mv /etc/systemd.journald.conf{.new,}
+fi
+
+# See: https://src.fedoraproject.org/rpms/glibc/pull-request/4
+# Basically that program handles deleting old shared library directories
+# mid-transaction, which never applies to rpm-ostree. This is structured as a
+# loop/glob to avoid hardcoding (or trying to match) the architecture.
+for x in /usr/sbin/glibc_post_upgrade.*; do
+    if test -f ${x}; then
+        ln -srf /usr/bin/true ${x}
+    fi
+done
