15c76e1ea62656b3e875251a3f62a8fa88d55d66
Martin Pitt <martin@piware.de>
2019-06-17 21:42:46 +0200

    Define tree for pitti's workstation
    Also add helper script for running the ostree build.

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
+Martin Pitt's desktop
+=====================
+
+This is an [rpm-ostree](https://coreos.github.io/rpm-ostree/) based minimal
+[Fedora](https://getfedora.org/) developer desktop with the [sway window manager](https://swaywm.org/) and [podman](https://podman.io/)/[toolbox](https://docs.fedoraproject.org/en-US/fedora-silverblue/toolbox/) for doing development and running less common graphical applications.
+
+It gets [automatically built](.github/workflows/build.yml) every week and [published as container image](https://github.com/martinpitt/ostree-pitti-workstation/pkgs/container/workstation-ostree-config), for using with [ostree native containers](https://coreos.github.io/rpm-ostree/container/).
+
+To use it from an existing OSTree based system like [Fedora CoreOS](https://getfedora.org/coreos) or [Fedora Silverblue](https://docs.fedoraproject.org/en-US/fedora-silverblue/), rebase your tree to it:
+
+```sh
+sudo rpm-ostree rebase ostree-unverified-registry:ghcr.io/martinpitt/workstation-ostree-config
+```
+
+After that, you can install weekly updates with
+
+```
+sudo rpm-ostree upgrade
+```
+
+If anything goes wrong, you can go back to the previous version with `sudo rpm-ostree rollback`.
+
+Login
+-----
+
+There is no graphical login manager. I log in on VT1, and my `.bashrc`
+automatically starts the GNOME SSH agent and sway:
+
+```sh
+if [ "$(tty)" = "/dev/tty1" ]; then
+    export `gnome-keyring-daemon --start --components=ssh`
+    export BROWSER=firefox-wayland
+    export XDG_CURRENT_DESKTOP=sway
+    exec sway > $XDG_RUNTIME_DIR/sway.log 2>&1
+fi
+```
+
+Original README for [workstation-ostree-config](https://pagure.io/workstation-ostree-config)
+=============================================
 # Manifests for rpm-ostree based Fedora variants
 
 This is the configuration needed to create
---
+++ pitti-post.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+# Enable SysRQ
+echo 'kernel.sysrq = 1' > /usr/lib/sysctl.d/90-sysrq.conf
+
+# power saving
+echo 'blacklist e1000e' > /usr/lib/modprobe.d/blacklist-local.conf
+
+# NetworkManager config
+cat <<EOF > /usr/lib/NetworkManager/conf.d/local.conf
+[main]
+plugins=
+
+[device]
+#wifi.backend=iwd
+EOF
+#ln -sfn ../iwd.service /usr/lib/systemd/system/multi-user.target.wants/iwd.service
+
+ln -sfn /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
+# set up PAM for systemd-homed
+authselect enable-feature with-systemd-homed
+
+# homed is missing a lot of SELinux policy (https://bugzilla.redhat.com/show_bug.cgi?id=1809878)
+# "disabled" breaks rpm-ostree (https://bugzilla.redhat.com/show_bug.cgi?id=1882933), so just use permissive
+sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
+
+# enable other units
+mkdir -p /usr/lib/systemd/system/getty.target.wants
+ln -s ../getty@.service /usr/lib/systemd/system/getty.target.wants/getty@tty1.service
+ln -s ../systemd-timesyncd.service /usr/lib/systemd/system/sysinit.target.wants/systemd-timesyncd.service
+ln -s ../systemd-resolved.service /usr/lib/systemd/system/multi-user.target.wants/systemd-resolved.service
+ln -s ../systemd-homed.service /usr/lib/systemd/system/multi-user.target.wants/systemd-homed.service
+ln -s ../cockpit.socket /usr/lib/systemd/system/sockets.target.wants/cockpit.socket
+ln -s ../sshd.socket /usr/lib/systemd/system/sockets.target.wants/sshd.socket
+
+# disable unwanted services
+ln -sfn /dev/null /usr/lib/systemd/user/at-spi-dbus-bus.service
+
+# move OS systemd unit defaults to /usr
+cp -a --verbose /etc/systemd/system /etc/systemd/user /usr/lib/systemd/
+rm -r /etc/systemd/system /etc/systemd/user
+
+# scanner permissions without scanner packages
+echo 'ACTION=="add|change", ENV{DEVTYPE}=="usb_device", ENV{ID_MODEL}=="CanoScan", MODE="666"' > /usr/lib/udev/rules.d/canoscan.rules
+
+# update for Red Hat certificate
+ln -s /etc/pki/ca-trust/source/anchors/2015-RH-IT-Root-CA.pem /etc/pki/tls/certs/2015-RH-IT-Root-CA.pem
+ln -s /etc/pki/ca-trust/source/anchors/2022-RH-IT-Root-CA.pem /etc/pki/tls/certs/2022-RH-IT-Root-CA.pem
+update-ca-trust
