6f56ff5d7f4fff5ebe0d5636896d5152146c1bd0
Martin Pitt <martin@piware.de>
2020-09-26 17:25:01 +0200

    Enable systemd-homed
    This requires some PAM config updates and disabling SELinux.

--- pitti-post.sh
+++ pitti-post.sh
@@ -19,11 +19,72 @@ EOF
 
 ln -sfn /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
+# set up PAM for systemd-homed (https://bugzilla.redhat.com/show_bug.cgi?id=1806949)
+authselect select minimal
+authselect opt-out
+patch /etc/pam.d/system-auth <<EOF
+--- /etc/pam.d/system-auth.orig
++++ /etc/pam.d/system-auth
+@@ -6,16 +6,20 @@
+ auth        required                                     pam_env.so
+ auth        required                                     pam_faildelay.so delay=2000000
+ auth        sufficient                                   pam_unix.so nullok
++-auth       sufficient                                   pam_systemd_home.so  # added
+ auth        required                                     pam_deny.so
+
+-account     required                                     pam_unix.so
++account     sufficient                                   pam_unix.so
++-account    sufficient                                   pam_systemd_home.so  # added
+
+ password    requisite                                    pam_pwquality.so
+ password    sufficient                                   pam_unix.so yescrypt shadow nullok use_authtok
++-password   sufficient                                   pam_systemd_home.so  # added
+ password    required                                     pam_deny.so
+
+ session     optional                                     pam_keyinit.so revoke
+ session     required                                     pam_limits.so
++-session    optional                                     pam_systemd_home.so  # added
+ -session    optional                                     pam_systemd.so
+ session     [success=1 default=ignore]                   pam_succeed_if.so service in crond quiet use_uid
+ session     required                                     pam_unix.so
+EOF
+patch /etc/pam.d/password-auth <<EOF
+--- password-auth
++++ password-auth
+@@ -6,16 +6,20 @@
+ auth        required                                     pam_env.so
+ auth        required                                     pam_faildelay.so delay=2000000
+ auth        sufficient                                   pam_unix.so nullok
++-auth       sufficient                                   pam_systemd_home.so  # added
+ auth        required                                     pam_deny.so
+
+-account     required                                     pam_unix.so
++account     sufficient                                   pam_unix.so
++-account    sufficient                                   pam_systemd_home.so  # added
+
+ password    requisite                                    pam_pwquality.so
+ password    sufficient                                   pam_unix.so yescrypt shadow nullok use_authtok
++-password   sufficient                                   pam_systemd_home.so  # added
+ password    required                                     pam_deny.so
+
+ session     optional                                     pam_keyinit.so revoke
+ session     required                                     pam_limits.so
++-session    optional                                     pam_systemd_home.so  # added
+ -session    optional                                     pam_systemd.so
+ session     [success=1 default=ignore]                   pam_succeed_if.so service in crond quiet use_uid
+ session     required                                     pam_unix.so
+EOF
+
+# homed is missing a lot of SELinux policy (https://bugzilla.redhat.com/show_bug.cgi?id=1809878)
+# "disabled" breaks rpm-ostree (https://bugzilla.redhat.com/show_bug.cgi?id=1882933), so just use permissive
+sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
+
 # enable other units
 mkdir -p /usr/lib/systemd/system/getty.target.wants
 ln -s ../getty@.service /usr/lib/systemd/system/getty.target.wants/getty@tty1.service
 ln -s ../systemd-timesyncd.service /usr/lib/systemd/system/sysinit.target.wants/systemd-timesyncd.service
 ln -s ../systemd-resolved.service /usr/lib/systemd/system/multi-user.target.wants/systemd-resolved.service
+ln -s ../systemd-homed.service /usr/lib/systemd/system/multi-user.target.wants/systemd-homed.service
 ln -s ../cockpit.socket /usr/lib/systemd/system/sockets.target.wants/cockpit.socket
 ln -s ../sshd.socket /usr/lib/systemd/system/sockets.target.wants/sshd.socket
 
